aac400a4de5c6d916a00b321c2f3467ebf5a5889
Mikołaj Kamiński <mkaminski321@gmail.com>
2020-12-03 15:01:11 +0100

    Day 03 solution

---
+++ 03/solution.rb
@@ -0,0 +1,21 @@
+INPUT = File.readlines("input.txt").map(&:strip)
+
+def slope_check(right, down)
+    INPUT
+        .each_with_index
+        .select{|x,i| i % down == 0}
+        .map {|x,i|
+            index = i / down * right % x.length
+            x[index] == "#"
+        }
+        .select {|x| x }
+        .count
+end
+
+# First part
+puts slope_check(3, 1)
+
+# Second part
+puts [[1,1], [3,1], [5,1], [7,1], [1,2]]
+    .map{|x| slope_check *x}
+    .inject(1, :*)
