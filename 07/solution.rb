a445cd7a0f33ddccb6aac676f6f4471ce51da04f
Mikołaj Kamiński <mkaminski321@gmail.com>
2020-12-07 15:31:10 +0100

    Day 07, part 2 solution

--- 07/solution.rb
+++ 07/solution.rb
@@ -5,14 +5,28 @@ INPUT = File.readlines("input.txt")
         { container => contents}
     }.inject(&:merge)
 
+# First part
 def search(target)
     INPUT
         .select{|key, val| val != nil && val.keys.include?(target) }
         .map{|key,val| [key] | search(key)}
 end
 
-# First part
 puts search("shiny gold")
     .flatten
     .uniq
+    .count
+
+#Second part
+def search2(target)
+    INPUT
+        .map{|key,val| val.nil? ? [key,{}] : [key,val]}
+        .to_h
+        .fetch(target)
+        .map{|key,val| [key]*val}
+        .flatten
+        .map{|x| [x].append search2(x)}
+        .flatten
+end
+puts search2("shiny gold")
     .count
