3844e5607946e9d8121edba457fbef059be87d4f
Mikołaj Kamiński <mkaminski321@gmail.com>
2020-12-08 08:08:09 +0100

    Day 08, part 2 solution

--- 08/solution.rb
+++ 08/solution.rb
@@ -1,28 +1,54 @@
-INPUT = File.readlines("input.txt").map(&:strip).map{|x| {"op"=>x.split.first,"val"=>x.split.last.to_i,"exec"=>false}}
+INPUT = File.readlines("input.txt").map(&:strip).map{|x| {"op"=>x.split.first,"val"=>x.split.last.to_i}}
+
+def run code
+    ptr = 0
+    acc = 0
+    exec = []
+    loop do
+        if exec.include?(ptr)
+            break
+        end
+        if ptr>=code.length
+            break
+        end
+        case code[ptr]["op"]
+        when "acc"
+            exec.push ptr
+            acc+=code[ptr]["val"]
+            ptr+=1
+            next
+        when "jmp"
+            exec.push ptr
+            ptr+=code[ptr]["val"]
+            next
+        when "nop"
+            exec.push ptr
+            ptr+=1
+        end
+    end
+    [acc, exec.include?(ptr)]
+end
 
 # First part
-ptr = 0
-acc = 0
-loop do
-    if INPUT[ptr]["exec"]
+puts run(INPUT).first
+
+# Second part
+def swap(i)
+    INPUT.each_with_index.map{|x,j|
+        if j==i
+            x["op"] == "jmp" ? {"op"=>"nop","val"=>x["val"]} : {"op"=>"jmp","val"=>x["val"]}
+        else
+            x
+        end
+    }
+end
+
+INPUT.each_with_index{|l,i|
+    next if l["op"]=="acc"
+    swp = swap(i)
+    acc, err = run swp
+    if !err
         puts acc
         break
     end
-    if ptr>=INPUT.length
-        break
-    end
-    case INPUT[ptr]["op"]
-    when "acc"
-        INPUT[ptr]["exec"] = true
-        acc+=INPUT[ptr]["val"]
-        ptr+=1
-        next
-    when "jmp"
-        INPUT[ptr]["exec"] = true
-        ptr+=INPUT[ptr]["val"]
-        next
-    when "nop"
-        INPUT[ptr]["exec"] = true
-        ptr+=1
-    end
-end
+}
