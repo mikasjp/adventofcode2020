70859b53dc27836a7c43867a2a8263a0a9914eb0
Mikołaj Kamiński <mkaminski321@gmail.com>
2020-12-09 11:15:07 +0100

    Day 09, second part solution

--- 09/solution.rb
+++ 09/solution.rb
@@ -1,15 +1,28 @@
 INPUT = File.readlines("input.txt").map(&:strip).map(&:to_i)
 
+invalid, count = INPUT
+.each_with_index
+.drop(25)
+.select{|x,i|
+    !INPUT[(i-25..i-1)]
+        .combination(2)
+        .map{|y| y.sum}
+        .include?(x)
+}
+.first
+
 # First part
-first_part = INPUT
-    .each_with_index
-    .drop(25)
-    .select{|x,i|
-            !INPUT[(i-2 5..i-1)]
-                .combination(2)
-                .map{|y| y.sum}
-                .include?(x)
+puts invalid
+
+# Second part
+puts (2..count)
+    .map{|x| [x, (0..count-x)]}
+    .map{|f,r|
+        r.map{|i|
+            res = INPUT.drop(i).take(f)
+            [res.sum==invalid, res.min + res.max]
+        }
+        .select{|success,result| success}
     }
-    .first
-    .first
-puts first_part
+    .flatten
+    .last
