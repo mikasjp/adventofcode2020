6fa2f455211584d2373e986f91d77a52180b4a82
Mikołaj Kamiński <mkaminski321@gmail.com>
2020-12-10 07:18:10 +0100

    Day 10, first part solution

---
+++ 10/solution.rb
@@ -0,0 +1,23 @@
+INPUT = File.readlines("input.txt").map{|x| x.strip.to_i}
+
+# First part
+used_adapters = [0]
+loop do
+    current_adapter = used_adapters.last
+    next_adapter = (INPUT-used_adapters)
+        .select{|x| x.between? current_adapter+1, current_adapter+3}
+        .min
+    break if next_adapter.nil?
+    used_adapters.append(next_adapter)
+end
+
+differences = used_adapters
+    .append(used_adapters.max+3)
+    .each_cons(2)
+    .map{|x,y| y-x}
+    .group_by{|x| x}
+    .map{|k,v| [k, v.count]}
+    .to_h
+
+    # First part
+puts differences.fetch(1,0)*differences.fetch(3,0)
