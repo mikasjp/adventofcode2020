466af4195c9b28f65e07936070f70e189063034f
Mikołaj Kamiński <mkaminski321@gmail.com>
2020-12-11 09:46:10 +0100

    Day 11, refactoring

--- 11/solution.rb
+++ 11/solution.rb
@@ -4,25 +4,32 @@ INPUT = File.readlines("input.txt")
     .flatten(2)
     .to_h
 
-# First part
-state = INPUT
-loop do
-    new_state = state.map{|k,v|
-        x,y = k
-        neighbors = (x-1..x+1)
-        .map{|n| (y-1..y+1).map{|m| [n,m]}}
-        .flatten(1).select{|n| n!=[x,y]}
-        .count{|n,m| state.fetch([n,m],nil)=="#" }
-        res = [k,v]
-        if v=="L" && neighbors==0
-            res = [k, "#"]
-        end
-        if v=="#" && neighbors>=4
-            res = [k, "L"]
-        end
-        res
-    }.to_h
-    break if state.inspect.hash == new_state.inspect.hash
-    state = new_state
+def solve count_neighbors
+    state = INPUT
+    loop do
+        new_state = state.map{|k,v|
+            x,y = k
+            neighbors = count_neighbors.call(x,y,state)
+            res = [k,v]
+            if v=="L" && neighbors==0
+                res = [k, "#"]
+            end
+            if v=="#" && neighbors>=4
+                res = [k, "L"]
+            end
+            res
+        }.to_h
+        break if state.hash == new_state.hash
+        state = new_state
+    end
+    state.count{|k,v| v=="#"}
 end
-puts state.count{|k,v| v=="#"}
+    
+# First part
+count_neighbors = lambda {|x,y,s|
+    (x-1..x+1)
+            .map{|n| (y-1..y+1).map{|m| [n,m]}}
+            .flatten(1).select{|n| n!=[x,y]}
+            .count{|n,m| s.fetch([n,m],nil)=="#" }
+}
+puts solve count_neighbors
