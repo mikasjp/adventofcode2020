5ba2c470d1bee5115de1bee1229f1452bab07468
Mikołaj Kamiński <mkaminski321@gmail.com>
2020-12-13 23:29:38 +0100

    Day 13, part 1 refactor

--- 13/solution.rb
+++ 13/solution.rb
@@ -1,9 +1,9 @@
-INPUT  = File.readlines("input.txt").map(&:strip).each_with_index.map{|x,i| i==0 ? x.to_i : x.split(",").map{|y| y=="x" ? "x" : y.to_i}}
+ts, INPUT  = File.readlines("input.txt").map(&:strip).each_with_index.map{|x,i| i==0 ? x.to_i : x.split(",").map{|y| y=="x" ? "x" : y.to_i}}
 
+# First part
 puts INPUT
-    .last
     .select{|x| x!="x"}
-    .map{|x| [x, (INPUT.first / x + 1)*x-INPUT.first ]}
+    .map{|x| [x, (ts / x + 1)*x-ts ]}
     .sort_by{|x,y| y}
     .map{|x,y| x*y}
     .first
