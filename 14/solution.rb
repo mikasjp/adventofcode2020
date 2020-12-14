84cfaf67ee815cd9708536e395fd2be7b6d8e6b8
Mikołaj Kamiński <mkaminski321@gmail.com>
2020-12-14 08:04:47 +0100

    Day 14, first part solution

--- 14/solution.rb
+++ 14/solution.rb
@@ -1,10 +1,18 @@
-MASK, INPUT = [File.open("input.txt", &:readline).strip.split(" = ").last.chars.each_with_index.select{|x,i|x!="X"}.map{|x,i|[i,x]}.group_by{|i,x|x}.map{|k,v|[k,v.map{|y|y.first}]}.to_h, File.read("input.txt").scan(/^mem\[(\d+)\] = (\d+)$/).map{|x|x.map{|y|y.to_i}}]
+INPUT = File.readlines("input.txt").map(&:strip)
 
 # First part
-or_mask = (0..35).map{|x|MASK["1"].include?(x) ? "1" : "0"}.join.to_i(2)
-and_mask = (0..35).map{|x|MASK["0"].include?(x) ? "0" : "1"}.join.to_i(2)
-puts INPUT
-    .group_by{|x,y| x}
-    .map{|k,v| v.last}
-    .map{|x,y| (y | or_mask) & and_mask}
-    .sum
+mem = {}
+or_mask = 0
+and_mask = 1
+INPUT
+    .each{|x|
+        if x.start_with? "mask"
+            mask = x.split(" = ").last
+            or_mask = mask.gsub("X","0").to_i(2)
+            and_mask = mask.gsub("X","1").to_i(2)
+        else
+            addr, val = x.scan(/^mem\[(\d+)\] = (\d+)$/).flatten.map(&:to_i)
+            mem[addr] = (val | or_mask) & and_mask
+        end
+    }
+puts mem.sum{|k,v| v}
